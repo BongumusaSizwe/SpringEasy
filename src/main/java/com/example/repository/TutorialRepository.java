5cc55fa0ff5912d2062c3c830430a85c25ec60e1
BongumusaSizwe <1682836@students.wits.ac.za>
2022-01-25 06:52:15 +0200

    Update

--- src/main/java/com/example/repository/TutorialRepository.java
+++ src/main/java/com/example/repository/TutorialRepository.java
@@ -8,3 +8,4 @@ public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
 	List<Tutorial> findByPublished(boolean published);
 	List<Tutorial>findByTitleContaining(String title);
 }
+
